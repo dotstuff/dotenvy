cb1be26eccf929dafe50c1c728cd6183aa625ca6
Jordan Sexton <jordan@jordansexton.com>
2015-06-12 01:22:58 -0500

    dotenvyyy

---
+++ index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var fs = require('fs');
+var dotenvy, load, parse;
+
+dotenvy = function (options, callback) {
+    var path     = '.env';
+    var encoding = 'utf8';
+    var file, env;
+
+    if (options != null) {
+        if ('path' in options) {
+            path = options.path;
+        }
+        if ('encoding' in options) {
+            encoding = options.encoding;
+        }
+    }
+
+    if (callback == null) {
+        file = fs.readFileSync(path, { encoding: encoding });
+        env  = parse(file, options);
+        load(env, options);
+    }
+    else {
+        fs.readFile(path, { encoding: encoding }, function (error, file) {
+            var env;
+            if (error == null) {
+                try {
+                    env = parse(file, options);
+                    load(env, options);
+                }
+                catch (error) {
+                    callback(error);
+                    return;
+                }
+            }
+            callback(error, env);
+        });
+    }
+};
+
+load = function (env, options) {
+    var overwrite = true;
+    var write;
+
+    if ('overwrite' in options) {
+        overwrite = options.overwrite;
+    }
+
+    if (overwrite) {
+        write = function (key) {
+            process.env[key] = env[key];
+        };
+    }
+    else {
+        write = function (key) {
+            if (!(key in process.env)) {
+                process.env[key] = env[key];
+            }
+        };
+    }
+
+    Object.keys(env).forEach(write);
+};
+
+parse = function (source, options) {
+    var env       = {};
+    var sigil     = '$';
+    var separator = '=';
+    var quote     = '"';
+    var split, quoted;
+
+    if (options != null) {
+        if ('sigil' in options) {
+            sigil = options.sigil;
+        }
+        if ('separator' in options) {
+            separator = options.separator;
+        }
+        if ('quote' in options) {
+            quote = options.quote;
+        }
+    }
+
+    split  = new RegExp('^\\s*([^' + separator + ']*?)\\s*' + separator + '\\s*(.*)?\\s*$');
+    quoted = new RegExp('^([' + quote + '])(\\.+)\\1$', 'g');
+
+    source.toString().split(/\n/).forEach(function (line, index) {
+        var match;
+        if (line.length > 0) {
+            match = line.match(split);
+            if (match == null) {
+                throw new SyntaxError('Invalid syntax on line ' + index + 1);
+            }
+            else {
+                var key   = match[1];
+                var value = match[2];
+                var variable;
+                if (value != null) {
+                    if (value[0] === sigil) {
+                        variable = value.substring(1);
+                        value    = env[variable] || process.env[variable];
+                    }
+                    else if (value.substring(0, 2) === '\\' + sigil) {
+                        value = value.substring(1);
+                    }
+                    else if (quoted.test(value)) {
+                        value = value.replace(quoted, '$2').replace(/\\n/g, '\n');
+                    }
+                }
+                env[key] = value;
+            }
+        }
+    });
+
+    return env;
+};
+
+module.exports = dotenvy;
+exports.load   = load;
+exports.parse  = parse;
